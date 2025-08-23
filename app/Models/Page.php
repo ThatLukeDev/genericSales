c185180bee5654fea075c57eab6ef67b97116cf2
ThatLukeDev <luke@l2j.co.uk>
2025-08-23 23:04:20 +0100

    page model

---
+++ app/Models/Page.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+	protected $fillable = [
+		"name",
+		"title",
+		"text",
+		"description"
+	];
+
+	public function get($name) {
+		$page = Page::where("name", $name)->firstOr(function() {
+			return Page::firstOrCreate(["name" => "error"], [
+				"name" => "error",
+				"title" => "404",
+				"text" => "Page not found",
+				"description" => "Please return home"
+			]);
+		});
+	}
+}
