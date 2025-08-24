0766f4246c19821a10404854cbd201617987b259
ThatLukeDev <luke@l2j.co.uk>
2025-08-24 19:54:02 +0100

    display & styling

--- resources/views/livewire/page.blade.php
+++ resources/views/livewire/page.blade.php
@@ -1,3 +1,5 @@
-<div>
-{{ $page->name }}
+<div class="m-10">
+	<h1 class="text-4xl">{{ $page->title }}</h1>
+	<h2 class="text-xl">{{ $page->text }}</h2>
+	<p class="">{{ $page->description }}</p>
 </div>
