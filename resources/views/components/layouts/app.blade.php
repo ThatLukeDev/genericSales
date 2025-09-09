590b13c5e700a763cff5454c856175fb909bb5cd
ThatLukeDev <luke@l2j.co.uk>
2025-09-09 08:05:16 +0100

    navbar frame

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,10 @@
 	<head>
 		@include("partials.head")
 	</head>
-	<body class="h-full">
-		{{ $slot }}
+	<body class="h-full flex flex-col">
+		<livewire:navbar />
+		<div class="flex-auto">
+			{{ $slot }}
+		</div>
 	</body>
 </html>
